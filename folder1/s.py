58011c6f7d96a45852a48e43859f86290beb8520
AyushRai09 <ayushrai982@gmail.com>
2017-03-20 02:46:05 +0530

    Made the functions with client now able to receive multiple requests of hash and index.

--- folder1/s.py
+++ folder1/s.py
@@ -1,5 +1,29 @@
 import socket,os
 
+def callLsOnClient(command):
+    conn.send(command)
+    lsResult=conn.recv(1024)
+    print lsResult
+
+def hashVerifyOnClient(filename, command):
+        filearg=filename
+        filearg="cksum"+" " + filename
+        hashValue=os.popen(filearg).read()
+        command=command + " " + hashValue
+        conn.send(command)
+        result=conn.recv(1024)
+        print result
+
+def DownloadRequestFromClient(filename):
+        f = open(filename,'rb')
+        l = f.read(1024)
+        while (l):
+           conn.send(l)
+           print('Sent ',repr(l))
+           l = f.read(1024)
+        f.close()
+        print('Done sending')
+
 port = 60000
 s = socket.socket()
 host = ""
@@ -9,43 +33,27 @@ s.listen(5)
 
 print 'Server listening....'
 
+conn, addr = s.accept()
 while True:
     command = raw_input("prompt:$ ")
 
     arg=command.split()
-    print "arg:",arg[0]
-    conn, addr = s.accept()
-    print 'Got connection from',addr
-    data = conn.recv(1024)
-    data=data.split()
-    print data
-    print('Server received', repr(data))
+    # print "arg:",arg[0]
+    # print 'Got connection from',addr
+    # data = conn.recv(1024)
+    # data=data.split()
+    # print data
+    # print('Server received', repr(data))
 
     if(arg[0] == 'index'):
-        conn.send(command)
-        lsResult=conn.recv(1024)
-        print lsResult
+        callLsOnClient(command)
 
     elif(arg[0] =='hash' and arg[1] =='verify'):
-        filearg=arg[2]
-        filearg="cksum"+" " + filearg
-        b=os.popen(filearg).read()
-        command=command + " " + b
-        conn.send(command)
-        result=conn.recv(1024)
-        print result
+        hashVerifyOnClient(arg[2],command)
 
     else:
-        print "arg in the else condition:",arg
-        f = open(arg[0],'rb')
-        l = f.read(1024)
-        while (l):
-           conn.send(l)
-           print('Sent ',repr(l))
-           l = f.read(1024)
-        f.close()
+        DownloadRequestFromClient(arg[0])
 
-        print('Done sending')
-    conn.send('Thank you for connecting')
-    conn.close()
+    # conn.send('Thank you for connecting')
+    # conn.close()
 s.close()
--- folder2/r.py
+++ folder2/r.py
@@ -1,42 +1,47 @@
 import socket,os
 
+def replyToCalledLsFromServer():
+        lsResult=os.listdir('.')
+        s.send(str(lsResult))
+        return
+
+def replyToCalledHashVerifyFromServer(filename, filehash):
+        filearg=filename
+        filearg="cksum" + " " + filearg
+        b=os.popen(filearg).read()
+        if(b==filehash):
+            s.send("True")
+        else:
+            s.send("False")
+def downloadFile(data):
+        f=open('testdata', 'wb')
+        print 'file opened'
+        print('data=%s', (data))
+        if not data:
+            return
+        f.write(str(data))
+        f.close()
+
 s = socket.socket()
 host = ""
 port = 60000
 
 s.connect((host, port))
-s.send("Hello server!")
+# s.send("Hello server!")
 
 while True:
     print('receiving data...')
     data = s.recv(1024)
     data=data.split()
-    print "data",data
-
+    print "Data:",data
     if(data[0] == 'index'):
-        lsResult=os.listdir('.')
-        s.send(str(lsResult))
-        break
+        replyToCalledLsFromServer()
 
     elif(data[0]=='hash' and data[1]=='verify'):
-        filearg=data[2]
-        filearg="cksum" + " " + filearg
-        b=os.popen(filearg).read()
-        if(b==data[3]):
-            s.send("True")
-        else:
-            s.send("False")
-        break
-    else:
-        f=open('testdata', 'wb')
-        print 'file opened'
-        print('data=%s', (data))
-        if not data:
-            break
-            # write data to a file
-        f.write(str(data))
+        replyToCalledHashVerifyFromServer(filename,data[3])
 
-        f.close()
+    else:
+        downloadFile(data)
 print('Successfully get the file')
 s.close()
 print('connection closed')
