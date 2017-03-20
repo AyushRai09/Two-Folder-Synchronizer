9523a64ea3429ace0cfb6051ab15b3c12861461e
AyushRai09 <ayushrai982@gmail.com>
2017-03-20 23:08:59 +0530

    Proper hash checking from client side done. Both cases of matching and non-matching hash matching done.

--- folder1/s.py
+++ folder1/s.py
@@ -1,9 +1,18 @@
 import socket,os
-
+foo=0
+arr=[[foo for i in range(100)] for j in range(100)]
 def callLsOnClient(command):
     conn.send(command)
     lsResult=conn.recv(1024)
-    print lsResult
+    lsResult=lsResult.split('\n')
+    i=0
+    del(lsResult[len(lsResult)-1]) #delte the last null character.
+    print "filename", "Time last modified"
+    for iterator in lsResult:
+        if(iterator.find('total')==-1):
+            arr[i]=iterator.split()
+            print arr[i][8], arr[i][5], arr[i][6], arr[i][7]
+            i=i+1
 
 def hashVerifyOnClient(filename, command):
         filearg=filename
@@ -12,7 +21,17 @@ def hashVerifyOnClient(filename, command):
         command=command + " " + hashValue
         conn.send(command)
         result=conn.recv(1024)
-        print result
+        if(result=="No changes made to the file."):
+            print result
+        else:
+            result=result.split()
+            print "Following files were modified:"
+            print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+            i=0
+            while(i<len(result)):
+                print result[i], result[i+1], result[i+2], result[i+3]
+                i=i+4
+        return
 
 def DownloadRequestFromClient(filename):
         f = open(filename,'rb')
@@ -26,8 +45,10 @@ def DownloadRequestFromClient(filename):
 
 port = 60000
 s = socket.socket()
+
 host = ""
 
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
 s.listen(5)
 
@@ -56,4 +77,5 @@ while True:
 
     # conn.send('Thank you for connecting')
     # conn.close()
+s.shutdown(socket.SHUT_RDWR)
 s.close()
--- folder2/r.py
+++ folder2/r.py
@@ -1,8 +1,16 @@
 import socket,os
-
+foo=0
+arr=[[foo for i in range(100)] for j in range(100)]
 def replyToCalledLsFromServer():
-        lsResult=os.listdir('.')
+        lsResult=os.popen('ls -l').read()
         s.send(str(lsResult))
+        lsResult=lsResult.split('\n')
+        i=0
+        del(lsResult[len(lsResult)-1]) #delte the last null character.
+        for iterator in lsResult:
+            if(iterator.find('total')==-1):
+                arr[i]=iterator.split()
+                i=i+1
         return
 
 def replyToCalledHashVerifyFromServer(filename, filehash):
@@ -10,9 +18,27 @@ def replyToCalledHashVerifyFromServer(filename, filehash):
         filearg="cksum" + " " + filearg
         b=os.popen(filearg).read()
         if(b==filehash):
-            s.send("True")
+            s.send("No changes made to the file.")
         else:
-            s.send("False")
+            lsResult=os.popen('ls -l').read()
+            lsResult=lsResult.split('\n')
+            i=0
+            del(lsResult[len(lsResult)-1]) #delte the last null character.
+            for iterator in lsResult:
+                if(iterator.find('total')==-1):
+                    arr[i]=iterator.split()
+                    i=i+1
+            string=""
+            i=0
+            while(i<100):
+                if(arr[i][8]!=0 and arr[i][5]!=0 and  arr[i][6]!=0 and arr[i][7]!=0):
+                    # print arr[i][8], arr[i][5], arr[i][6], arr[i][7]
+                    string=string + arr[i][8] + " " + arr[i][5] + " " + arr[i][6] + " " + arr[i][7] + " "
+                i=i+1
+            s.send(string)
+        return
+
+
 def downloadFile(data):
         f=open('testdata', 'wb')
         print 'file opened'
